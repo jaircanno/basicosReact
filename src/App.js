f1d1e7654773e43511846e2a9ae41b8d22d792fa
Eduardo Jair Cano <jair.ci@incfile.com>
2019-07-08 21:57:47 -0500

    Variables de Entrada y Salida, Primer Class Component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/* ----- Libraries ---------------------------------------------------- */
+import React, {Fragment} from 'react';
+import PrimerComponente from './components/PrimerComponente.js'
 
 function App() {
+
+    /* ----- Variables -------------------------------------- */
+    const empleado = {
+        nombre: 'Jair Cano',
+        trabajo : 'Desarrollador FullStack'
+    };
+
+    /* ----- Views Elements --------------------------------- */
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <Fragment>
+          < PrimerComponente />
+          <h2> {empleado.nombre} </h2>
+          <p> {empleado.trabajo} </p>
+          { 2 + 2 }
+      </Fragment>
+  )
+
 }
 
 export default App;
---
+++ src/components/PrimerComponente.js
@@ -0,0 +1,11 @@
+/* ----- Libraries ---------------------------------------------------- */
+import React, { Component } from 'react';
+
+/* ----- First Class Component ------------------------------ */
+class  PrimerComponente extends Component{
+    render() {
+        return <h1>Hola mundo</h1>
+    }
+}
+/* ----- Export Class Component ----------------------------- */
+export default  PrimerComponente;
